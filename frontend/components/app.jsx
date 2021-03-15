ec70cf73d66039a0668e939058e091854c90f57f
your name here <joshuaebrani@gmail.com>
2021-03-15 18:57:21 -0400

    Edit root

---
+++ frontend/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
+
+
+const App = () => {
+    return(
+        <div>
+            <h1>WE ARE LIVE</h1>
+        </div>
+    )
+}
+
+export default App
