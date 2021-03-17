cba05ff8a9a9970eb08535334b44dfed839ba228
your name here <joshuaebrani@gmail.com>
2021-03-17 15:43:00 -0400

    signup css

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -29,92 +29,96 @@ class Signup extends React.Component {
     render() {
         return (
           <section className="modal-background">
-            <form className='modal-child' className='signup-form'>
-              <h1>Sign Up</h1>
-              <p>It's quick and easy.</p>
+            <form className="modal-child">
+              <div className="signup-form">
+                  <div className='sign-head'>
+                <h1 className='su'>Sign Up</h1>
+                <p className='easy'>It's quick and easy.</p>
+                  </div>
 
-              <div className="text">
-                <div className="fullname">
-                  <input
-                    type="text"
-                    name="fname"
-                    placeholder="First name"
-                    onChange={this.handleInput("fname")}
-                    value={this.state.fname}
-                  />
-
-                  <input
-                    type="text"
-                    name="lname"
-                    placeholder="Last name"
-                    onChange={this.handleInput("lname")}
-                    value={this.state.lname}
-                  />
-                </div>
-
-                <div className="oneline">
-                  <div className="email">
+                <div className="text">
+                  <div className="fullname">
                     <input
                       type="text"
-                      name="email"
-                      placeholder="Email"
-                      onChange={this.handleInput("email")}
-                      value={this.state.email}
+                      name="fname"
+                      placeholder="First name"
+                      onChange={this.handleInput("fname")}
+                      value={this.state.fname}
+                    />
+
+                    <input
+                      type="text"
+                      name="lname"
+                      placeholder="Last name"
+                      onChange={this.handleInput("lname")}
+                      value={this.state.lname}
                     />
                   </div>
 
-                  <div className="pass">
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="Create new password"
-                      onChange={this.handleInput("password")}
-                      value={this.state.password}
-                    />
+                  <div className="oneline">
+                    <div className="email">
+                      <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        onChange={this.handleInput("email")}
+                        value={this.state.email}
+                      />
+                    </div>
+
+                    <div className="pass">
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="Create new password"
+                        onChange={this.handleInput("password")}
+                        value={this.state.password}
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="bday">
-                <input
-                  type="date"
-                  name="birthday"
-                  placeholder="1/1/2021"
-                  onChange={this.handleInput("birthday")}
-                  value={this.state.birthday}
-                />
-              </div>
-
-              <div className="gender">
-                <label>
-                  Male
+                <div className="bday">
                   <input
-                    type="radio"
-                    name="gender"
-                    value="Male"
-                    onChange={this.handleInput("gender")}
-                    value={this.state.gender}
+                    type="date"
+                    name="birthday"
+                    placeholder="1/1/2021"
+                    onChange={this.handleInput("birthday")}
+                    value={this.state.birthday}
                   />
-                </label>
-                <label>
-                  Female
-                  <input
-                    type="radio"
-                    name="gender"
-                    value="Female"
-                    onChange={this.handleInput("gender")}
-                    value={this.state.gender}
-                  />
-                </label>
+                </div>
+
+                <div className="gender">
+                  <label>
+                    Male
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="Male"
+                      onChange={this.handleInput("gender")}
+                      value={this.state.gender}
+                    />
+                  </label>
+                  <label>
+                    Female
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="Female"
+                      onChange={this.handleInput("gender")}
+                      value={this.state.gender}
+                    />
+                  </label>
+                </div>
+
+                <p className="footer">
+                  By clicking Sign Up, you agree to our Terms, Data Policy and
+                  Cookies Policy. You may receive SMS Notifications from us and
+                  can opt out any time.
+                </p>
+
+                <button onClick={this.handleSubmit} className='new'>Sign Up</button>
               </div>
-
-              <p className="footer">
-                By clicking Sign Up, you agree to our Terms, Data Policy and
-                Cookies Policy. You may receive SMS Notifications from us and
-                can opt out any time.
-              </p>
-
-              <button onClick={this.handleSubmit}>Sign Up</button>
             </form>
           </section>
         );
