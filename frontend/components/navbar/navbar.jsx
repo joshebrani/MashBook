c335b1ea14f3870d31e20d1a338f14a0edb71c1e
your name here <joshuaebrani@gmail.com>
2021-03-18 17:18:24 -0400

    navbar styling

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -16,25 +16,41 @@ class NavBar extends React.Component {
 
   render() {
     return (
+      <div>
         <div className="nav-top">
+          <div className="m-s">
+            <img className="m-icon" src={window.m} alt="" />{" "}
+            <img className="search-icon" src={window.search} alt="" />{" "}
+          </div>
 
-        <div className='m-s'>
-        <img className="m-icon" src={window.m} alt="" />{" "}
-        <img className="search-icon" src={window.search} alt="" />{" "}
+          <div className="h-v-g">
+            <img className="home-icon" src={window.home} alt="" />{" "}
+            <img className="video-icon" src={window.video} alt="" />{" "}
+            <img className="group-icon" src={window.group} alt="" />{" "}
+          </div>
+
+          <div className="m-b-d">
+            <img className="messages-icon" src={window.messages} alt="" />{" "}
+            <img className="bell-icon" src={window.bell} alt="" />{" "}
+            <img className="down-icon" src={window.down} alt="" />{" "}
+          </div>
+          <button onClick={this.logout}>Log Out</button>
         </div>
 
-        <div className='h-v-g'>
-        <img className="home-icon" src={window.home} alt="" />{" "}
-        <img className="video-icon" src={window.video} alt="" />{" "}
-        <img className="group-icon" src={window.group} alt="" />{" "}
+        <div className="nav-side">
+          {/* <p>profile</p> */}
+          <p>
+            <img className="linkedin-icon" src={window.linkedin} alt="" />{" "}
+            Linkedin
+          </p>
+          <p>
+            <img className="github-icon" src={window.github} alt="" /> Github
+          </p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
         </div>
-
-        <div className='m-b-d'>
-        <img className="messages-icon" src={window.messages} alt="" />{" "}
-        <img className="bell-icon" src={window.bell} alt="" />{" "}
-        <img className="down-icon" src={window.down} alt="" />{" "}
-        </div>
-        {/* <button onClick={this.logout}>Log Out</button> */}
       </div>
     );
   }
