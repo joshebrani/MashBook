26925923059b3285807ff0e45cabac51f038a3b7
your name here <joshuaebrani@gmail.com>
2021-03-25 18:38:16 -0400

    attempt to get posts earlier

--- frontend/components/newsfeed/user_sidebar.jsx
+++ frontend/components/newsfeed/user_sidebar.jsx
@@ -8,9 +8,9 @@ class UserSidebar extends React.Component {
 
     }
 
-    // componentDidMount() {
-    //     this.props.getUsers();
-    // }
+    componentDidMount() {
+        this.props.getUsers().then(() => this.props.getPosts());
+    }
 
     
     render() {
