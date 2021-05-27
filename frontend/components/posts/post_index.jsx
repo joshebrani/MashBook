b3f9abd358125854b9152fa5f307abd253969f0f
your name here <joshuaebrani@gmail.com>
2021-05-27 14:58:07 -0400

    css spinner

--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -20,32 +20,36 @@ class PostIndex extends React.Component {
         return (
           <div>
             <ul className="after-post">
-              {posts.length > 0 ? posts.reverse().map(post => (
-                // <div key={post.id}>
-                //   <p>
-                //     {authors[post.post_author_id].fname +
-                //       " " +
-                //       authors[post.post_author_id].lname}
-                //   </p>
-                //     <p>
-                //   {post.body}
-                //      </p>
-                //   <button onClick={() => destroyPost(post.id)}>
-                //     Delete Post
-                //   </button>
-                // </div>
-                <PostIndexItem
-                post={post}
-                currentUser={currentUser}
-                destroyPost={destroyPost}
-                authors={authors}
-                key={post.id}
-                openModal={openModal}
-                closeModal={closeModal}
-                getPosts={getPosts}
-                getUsers={getUsers}
-                />
-              )) : 'Loading...'}
+              {posts.length > 0 ? (
+                posts.reverse().map((post) => (
+                  // <div key={post.id}>
+                  //   <p>
+                  //     {authors[post.post_author_id].fname +
+                  //       " " +
+                  //       authors[post.post_author_id].lname}
+                  //   </p>
+                  //     <p>
+                  //   {post.body}
+                  //      </p>
+                  //   <button onClick={() => destroyPost(post.id)}>
+                  //     Delete Post
+                  //   </button>
+                  // </div>
+                  <PostIndexItem
+                    post={post}
+                    currentUser={currentUser}
+                    destroyPost={destroyPost}
+                    authors={authors}
+                    key={post.id}
+                    openModal={openModal}
+                    closeModal={closeModal}
+                    getPosts={getPosts}
+                    getUsers={getUsers}
+                  />
+                ))
+              ) : (
+                <div class="lds-hourglass"></div>
+              )}
             </ul>
           </div>
         );
